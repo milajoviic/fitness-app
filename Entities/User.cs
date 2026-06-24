a1daac75ffa3ad6bb00090843b451e47b6cfaafc
Mila Jovic <mila.jovic@elfak.rs>
2026-06-24 22:31:06 +0200

    aa

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Linq;
-using System.Text;
-using FitnessApp.Enums;
+﻿using FitnessApp.Enums;
 
 namespace FitnessApp.Entities
 {
     public class User
     {
-        private string FirstName { get; set; }
-        private string LastName { get; set; }
-        private string Email { get; set; }
-        private GenderEnum Gender { get; set; }
-        private int Age { get; set; }
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public GenderEnum Gender { get; set; }
+        public string PasswordHash { get; set; } = "";
+        public DateTime BirthDate { get; set; }
+        public DateTime? LastPeriodDate { get; set; }
     }
 }
--- Entities/UserDiet.cs
+++ Entities/UserDiet.cs
@@ -2,5 +2,12 @@
 {
     public class UserDiet
     {
+        public string Breakfast { get; set; }
+        public string Lunch { get; set; }
+        public string Dinner { get; set; }
+        public List<string>? Snacks { get; set; }
+        public List<string>? Additional { get; set; } //moze da se napravi i kao enumeracija sa navedenim dodacima
+        public DateTime Day { get; set; }
+        public int? Calories { get; set; }
     }
 }
